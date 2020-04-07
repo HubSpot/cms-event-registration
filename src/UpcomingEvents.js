002a9f93161a1affc34d1a52e77a945419d1065b
levinkeo <kevina.leonard@gmail.com>
2020-04-07 16:02:35 -0400

    update imports to bring in needed polyfills for async/wait

--- src/UpcomingEvents.js
+++ src/UpcomingEvents.js
@@ -1,3 +1,5 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import { AppContext, AppProvider } from './AppContext';

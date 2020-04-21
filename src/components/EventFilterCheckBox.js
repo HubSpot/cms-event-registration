4e4522b6898a06a23014e3cc7bdd0c7537b92446
Allison Kemmerle <akemmerle@hubspot.com>
2020-04-21 03:18:51 -0400

    Add component to control checkboxes & associated state

---
+++ src/components/EventFilterCheckBox.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const EventFilterCheckBox = ({ property, propertyType, addEventFilter, removeEventFilter }) => {
+  const [check, setCheck] = useState(false);
+
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  const handleClick = (property) => {
+    setCheck(!check);
+
+    if (check) {
+      removeEventFilter(propertyType, property)
+    } else {
+      addEventFilter(propertyType, property)
+    }
+  }
+
+  return (
+    <li className="filter-bar__browse--dropdown-list-item">
+      <input
+        type="checkbox"
+        id={property}
+        checked={check}
+        onClick={() => handleClick(property)}
+      />
+      <label for={property} data-content={capitalize(property)}>
+      </label>
+    </li>
+  )
+}
+
+export default EventFilterCheckBox;

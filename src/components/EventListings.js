91a309148c1f92f920bfcfd51f861c737b263c4b
Allison Kemmerle <akemmerle@hubspot.com>
2020-04-21 03:19:21 -0400

    Modify EventListings to display filtered events

--- src/components/EventListings.js
+++ src/components/EventListings.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import EventCard from './EventCard';
 import './EventListings.scss';
+import _a from 'lodash/array';
+import _c from 'lodash/collection';
+import _l from 'lodash/lang';
 
-const EventListings = ({ events, currentSearch }) => {
+const EventListings = ({ events, currentSearch, filteredEventProperties }) => {
   let filteredEvents = events;
 
   if (currentSearch) {
     filteredEvents = filteredEvents.filter(event =>
       event.values.name.toLowerCase().includes(currentSearch.toLowerCase()),
+    );
+  }
+
+  if (filteredEventProperties.length > 0) {
+    filteredEvents = filteredEvents.filter(e =>
+      _a.intersectionWith([{type: "location",
+                            property: e.values.location_address}].
+                        concat(_c.map(e.values.type, t => ({type: "type", property: t.name}))),
+                      filteredEventProperties, _l.isEqual).length > 0
     );
   }
 

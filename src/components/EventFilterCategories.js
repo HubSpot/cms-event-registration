50a4aa0b2314f8dba16febb564d67c909165d047
Allison Kemmerle <akemmerle@hubspot.com>
2020-04-24 23:00:58 -0400

    Run ESLint

--- src/components/EventFilterCategories.js
+++ src/components/EventFilterCategories.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import EventFilterCheckBox from './EventFilterCheckBox';
 import './EventFilterCategories.scss';
 
--- src/components/EventFilterColumn.js
+++ src/components/EventFilterColumn.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import EventFilterCategories from './EventFilterCategories';
 import './EventFilterColumn.scss';
 import _ from 'lodash/array';

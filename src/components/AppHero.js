b1604c4b62cc55a517560a9ff52114a0e5068374
levinkeo <16004483+levinkeo@users.noreply.github.com>
2020-06-04 16:58:20 -0400

    move AppHero styles into new scss file

--- src/components/AppHero.js
+++ src/components/AppHero.js
@@ -3,6 +3,7 @@ import { AppContext } from '../AppContext.js';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import './AppHero.scss';
 
 const AppHero = () => {
   const [state] = useContext(AppContext);

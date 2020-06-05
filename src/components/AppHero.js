3b90bf33c3946a47ecd194ff6b582a2c983ed53b
levinkeo <16004483+levinkeo@users.noreply.github.com>
2020-06-05 13:10:02 -0400

    drop .js

--- src/components/AppHero.js
+++ src/components/AppHero.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { AppContext } from '../AppContext.js';
+import { AppContext } from '../AppContext';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';

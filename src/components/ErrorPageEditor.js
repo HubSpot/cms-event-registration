0b1f1d97528339217c97cec588eb47180a5b0af1
Allison Kemmerle <akemmerle@hubspot.com>
2020-06-08 23:50:12 -0400

    Streamline renderError function & add code comment

--- src/components/ErrorPageEditor.js
+++ src/components/ErrorPageEditor.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+//This component will only render in the HubSpot page editor.
 
 const ErrorPageEditor = ({ events, portalId, tableId }) => {
   function renderErrors() {
-    if (events) {
-      if (
-        events.find(e => e.name === '' || e.path === '') &&
-        window.hsInEditor
-      ) {
-        return (
-          <>
-            <h1>
-              Warning: Event with ID{' '}
-              {events.find(e => e.name === '' || e.path === '').id} in the HubDb
-              table is missing a page title or page path.
-            </h1>
-            <h2>
-              To fix this,{' '}
-              <Link to={`//app.hubspot.com/hubdb/${portalId}/table/${tableId}`}>
-                edit the Events HubDB table
-              </Link>
-            </h2>
-          </>
-        );
-      }
+    if (
+      events &&
+      events.find(e => e.name === '' || e.path === '') &&
+      window.hsInEditor
+    ) {
+      return (
+        <>
+          <h1>
+            Warning: Event with ID{' '}
+            {events.find(e => e.name === '' || e.path === '').id} in the HubDb
+            table is missing a page title or page path.
+          </h1>
+          <h2>
+            To fix this,{' '}
+            <Link to={`//app.hubspot.com/hubdb/${portalId}/table/${tableId}`}>
+              edit the Events HubDB table
+            </Link>
+          </h2>
+        </>
+      );
     }
   }
 
